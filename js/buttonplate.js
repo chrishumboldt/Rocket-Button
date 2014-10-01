e2909d4447c0f53f210db92cf75a98f8bb003c38
Chris Humboldt <chrismodem@gmail.com>
2014-10-01 18:25:47 +0200

    General Updates
    - Changed the plugin structure.
    - Updated to work on dynamic pages as well.

--- js/buttonplate.js
+++ js/buttonplate.js
@@ -1,87 +1,108 @@
 /**
  * jQuery File: 	buttonplate.js
- * Type: 			plugin
+ * Type:			plugin
  * Author:        	Chris Humboldt
- * Last Edited:   	22 March 2014
+ * Last Edited:   	1 October 2014
  */
 
 
 // Plugin
-(function($){
-
-	var $buttonplate						= function(){
-		
-		// ------------------------------------------------ SETUP
-		var $object 						= this;
-		var $btn_width						= 0;
-		var $this_button					= false;
-		
-
-		// ------------------------------------------------ SETTINGS
-		$object.settings 					= {};
-		
-
-		// ------------------------------------------------ INITIALIZE
-		$object.init 						= function($element, settings){
-			
-			// Check if the settings are being edited via the call
-			$object.settings 				= $.extend($object.settings, settings);
-			
-			// Some variables
-			$this_button					= $element;
-			
-			// Close any drop downs
-			$('html').on('click', function(){
-				
-				$('.button-drop-down-open').removeClass('button-drop-down-open').find('ul').hide();
-			});
-			
-			// Set list options / Show list function
-			if($this_button.find('ul').length > 0){
-
-				$this_button.addClass('button-drop-down');
-				
-				$(window).resize(function(){
-				
-					$object.size_drop_down();
-				});
-				$object.show_drop_down();
-			}
-		}
-		
-
-		// ------------------------------------------------ FUNCTIONS
-		// Show drop-down
-		$object.show_drop_down				= function(){
-			
-			$this_button.on('click', function(){
-				
-				$object.size_drop_down();
-				$this_button.find('ul').fadeIn('fast', function(){
-					
-					$this_button.addClass('button-drop-down-open');
-				});
-			});
-		}
-		
-		// Size drop-down
-		$object.size_drop_down				= function(){
-			
-			$btn_width					= $this_button.outerWidth();
-			$this_button.find('ul').width($btn_width);
-		}
+;(function($, window, document, undefined)
+{
+	// Plugin setup & settings
+	var $plugin_name					= 'buttonplate', $defaults =
+	{
 	};
 	
-	// Call the plugin
-	$.fn.buttonplate						= function($options){
-		
-		var len = this.length;
+	// The actual plugin constructor
+	function Plugin($element, $options) 
+	{
+		this.element 					= $element;
+		this.settings 					= $.extend({}, $defaults, $options);
+		this._defaults 					= $defaults;
+		this._name 						= $plugin_name;
 
-		return this.each(function(index) {
-			
-			var me = $(this), key = 'buttonplate' + (len > 1 ? '-' + ++index : ''), instance = (new $buttonplate).init(me, $options);
-			me.data(key, instance).data('key', key);
+		// Initilize plugin
+		this.init();
+	}
+	
+	// Plugin
+	// ---------------------------------------------------------------------------------------
+	Plugin.prototype 					= 
+	{
+		init 							: function()
+		{
+			// Variables
+			// ---------------------------------------------------------------------------------------
+			var $this 					= this;
+			var $settings 				= $this.settings;
+			var $button 				= $($this.element);
+
+
+			// Execute
+			// ---------------------------------------------------------------------------------------
+			fc_button_setup();
+
+
+			// Functions
+			// ---------------------------------------------------------------------------------------
+			// Button drop size
+			function fc_button_setup()
+			{
+				if($button.find('ul').length > 0)
+				{
+					$button.addClass('button-drop-down');
+				}
+			};
+		}
+	};
+
+
+	// Global calls
+	// ---------------------------------------------------------------------------------------
+	// On click show
+	$(document).on('click', '.button-drop-down', function()
+	{
+		var $button 					= $(this);
+		var $button_w					= $button.outerWidth();
+
+		// Show drop down
+		$button.find('ul').width($button_w).fadeIn('fast', function()
+		{	
+			$button.addClass('button-drop-down-open');
 		});
+	});
+
+	// Close drop-downs
+	$(document).on('click', function(){
+		
+		$('.button-drop-down-open').removeClass('button-drop-down-open').find('ul').hide();
+	});
+
+	// On resize
+	$(window).resize(function()
+	{
+		$('.button-drop-down-open').removeClass('button-drop-down-open').find('ul').hide();
+	});
+
+
+	// Plugin wrapper
+	// ---------------------------------------------------------------------------------------
+	$.fn[$plugin_name] 					= function($options)
+	{
+		var $plugin;
+
+		this.each(function()
+		{
+			$plugin 					= $.data(this, 'plugin_' + $plugin_name);
+
+			if(!$plugin)
+			{
+				$plugin 				= new Plugin(this, $options);
+				$.data(this, 'plugin_' + $plugin_name, $plugin);
+			}
+		});
+
+		return $plugin;
 	};
-	
-}(jQuery));
+})(jQuery, window, document);
