8af2cf25a317c0f8ba2e285d4ac5cd2d2ad56348
Chris Humboldt <chrismodem@gmail.com>
2015-04-29 14:24:00 +0200

    Removed jQuery dependency. Rewrote JS.

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['watch']);
 	grunt.registerTask('build', ['sass', 'uglify']);
-	
+
 	// Initialize config
 	grunt.initConfig({
 		// Package
@@ -23,14 +23,13 @@ module.exports = function(grunt) {
 					'css/demo.css': 'sass/demo.scss'
 				}
 			}
-		}, 
+		},
 		// Uglify
 		uglify: {
 			my_target: {
 				files: {
-					'js/min/jquery-v1.10.2.min.js'			: ['js/jquery-v1.10.2.js'],
-					'js/min/modernizr-custom-v2.8.3.min.js'	: ['js/modernizr-custom-v2.8.3.js'],
-					'js/min/buttonplate.min.js'				: ['js/buttonplate.js']
+					'js/min/modernizr-custom-v2.8.3.min.js': ['js/modernizr-custom-v2.8.3.js'],
+					'js/min/buttonplate.min.js': ['js/buttonplate.js']
 				}
 			}
 		},
@@ -51,9 +50,9 @@ module.exports = function(grunt) {
 			// Live reload
 			options: {
 				livereload: true,
-		    } 
-		    // End of live reload
-		} 
+			}
+			// End of live reload
+		}
 		// End of watch
 	});
 	// End of initialize config
